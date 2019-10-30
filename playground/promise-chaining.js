4f8e294667e54915f43b403e3739520c094851e5
Nicole <nicoleotieno18@gmail.com>
2019-10-30 16:48:35 +0300

    create promise-chaining.js file

---
+++ playground/promise-chaining.js
@@ -0,0 +1,11 @@
+require('../src/db/mongoose');
+const User = require('../src/models/user');
+
+User.findByIdAndUpdate('5db2f3b96da2571b40a91947', { age: 19 }).then((user) => {
+    console.log(user);
+    return User.countDocuments({ age: 19 });
+}).then((result) => {
+    console.log(result);
+}).catch((e) => {
+    console.log(e);
+});
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require ('express');
 require('./db/mongoose');
 const User = require('./models/user');
-const Task = require('./model/task');
+const Task = require('./models/task');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; //specifying the port that will be used by server & if it doesn't exist 3000 is used
 
-app.use(express.json());
+app.use(express.json()); //use express to format data 
 
 app.post('/users', (req, res) => {
     const user = new User(req.body);
@@ -15,6 +15,60 @@ app.post('/users', (req, res) => {
         res.status(201).send(user);
     }).catch((error) => {
         res.status(400).send(error);
+    });
+});
+
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users);
+    }).catch((error) => {
+        res.status(500).send();
+    })
+});
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+
+    User.findById(_id).then((user) => {
+        if(!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    }).catch((e) => {
+        res.status(500).send();
+    });
+});
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body);
+
+    task.save().then(() => {
+        res.status(201).send(task);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks);
+    }).catch((e) => {
+        res.status(500).send();
+    });
+});
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        res.send(task);
+    }).catch((e) => {
+        res.status(500).send();
     });
 });
 
