2aff11d10b35e44b3293a3f151fb1cc639d78433
Nicole <nicoleotieno18@gmail.com>
2019-11-01 10:47:32 +0300

    update age and count

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,11 +1,21 @@
 require('../src/db/mongoose');
 const User = require('../src/models/user');
 
-User.findByIdAndUpdate('5db2f3b96da2571b40a91947', { age: 19 }).then((user) => {
-    console.log(user);
-    return User.countDocuments({ age: 19 }); //countDocument counts the number of documents updated
-}).then((result) => {
-    console.log(result);
-}).catch((e) => {
-    console.log(e);
+// User.findByIdAndUpdate('5db2f3b96da2571b40a91947', { age: 19 }).then((user) => {
+//     console.log(user);
+//     return User.countDocuments({ age: 19 }); //countDocument counts the number of documents updated
+// }).then((result) => {
+//     console.log(result);
+// }).catch((e) => {
+//     console.log(e);
+// });
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age });
+    const count = await User.countDocuments({ age });
+    return count;
+};
+
+updateAgeAndCount('5db2f3b96da2571b40a91947', 21).then((count) => {
+    console.log(count);
 });
