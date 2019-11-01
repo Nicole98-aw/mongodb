50469b6094c8dfb243462a7a15e916a69db193b9
Nicole <nicoleotieno18@gmail.com>
2019-11-01 11:46:10 +0300

    delete task

---
+++ playground/promise-chaining3.js
@@ -0,0 +1,22 @@
+// Goal: Use async await
+// 1. Create deleteTaskAndCount as an async function
+// Accept id of task to remove
+// 2. Use await to delete task and count up incomplete tasks
+// 3. Return the count
+// 4. Call the function and attach then/catch to log results
+// 5. Test your work
+require('../src/db/mongoose');
+const Task = require('../src/models/task');
+
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({ completed: false });
+    return count;
+};
+
+deleteTaskAndCount('5db2973e68b27d225c472a34').then((count) => {
+    console.log(count);
+}).catch((e) => {
+    console.log(e);
+});
