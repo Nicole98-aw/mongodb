650191fa74858277ead36e36a609355d85c7815a
Nicole <nicoleotieno18@gmail.com>
2019-11-05 17:29:03 +0300

    assign token to a user

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const taskRouter = require('./routers/task');
 const app = express(); //instantiating express app
 const port = process.env.PORT || 3000; //specifying the port that will be used by server & if it doesn't exist 3000 is used
 
+// app.use((req, res, next) => {
+//     res.status(503).send('Site is currently down');
+// });
+
 app.use(express.json()); //use express to format data
 app.use(userRouter);
 app.use(taskRouter); 
@@ -15,17 +19,29 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
 
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
+
+// const myFunction = async () => {
+//     const password = 'batman';
+//     const hashedPassword = await bcrypt.hash(password, 8);
+
+//     console.log(password);
+//     console.log(hashedPassword);
+
+//     const isMatch = await bcrypt.compare('batman', hashedPassword);
+//     console.log(isMatch);
+// };
+
+// myFunction();
+
+const jwt = require('jsonwebtoken');
 
 const myFunction = async () => {
-    const password = 'batman';
-    const hashedPassword = await bcrypt.hash(password, 8);
+    const token = jwt.sign({ _id: 'abc123'}, 'naughty', { expiresIn: '10 seconds'});
+    console.log(token);
 
-    console.log(password);
-    console.log(hashedPassword);
-
-    const isMatch = await bcrypt.compare('batman', hashedPassword);
-    console.log(isMatch);
-};
+    const data = jwt.verify(token, 'naughty');
+    console.log(data);
+}
 
 myFunction();
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
 
@@ -19,7 +20,21 @@ router.post('/users', async (req, res) => { // path for creating through /users
     }
 });
 
-router.get('/users', async (req, res) => { //path for retrieving data through /users
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken();
+        res.send({ user, token });
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user);
+});
+
+router.get('/users', auth, async (req, res) => { //path for retrieving data through /users
     // User.find({}).then((users) => {
     //     res.send(users);
     // }).catch((error) => {
@@ -67,8 +82,13 @@ router.get('/users/:id', async (req, res) => {
         }
 
         try {
-            const user = await User.findByIdAndUpdate(req.params.id,
-            req.body, { new: true, runValidators: true });
+            const user = await User.findById(req.params.id);
+
+            updates.forEach((update) => {
+                return user[update] = req.body[update];
+            });
+
+            await user.save();
 
             if (!user) {
                 return res.status(404).send();            
